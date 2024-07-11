9ea8e48d52ae26f7f8994298ac1b7b103ce371ac
hvjha <119492741+hvjha@users.noreply.github.com>
2024-07-12 00:59:44 +0530

    Update index.js
    Backend commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({origin:['https://vercel.com/harsh-vardhan-jhas-projects/flight-mern/jEUMM8TazickrXDMTqGDCjfXJbaM']));
 app.use(express.json());
 
 app.post('/api/search', async (req, res) => {
